1ca5e3a729f1637d508f25f0624deb92c604ebc1
Kirill Byvshev <k@byv.sh>
2023-09-02 13:38:28 +0400

    tests for validate credentials operation

--- omgservers-user/src/main/java/com/omgservers/module/user/impl/operation/validateCredentials/ValidateCredentialsOperation.java
+++ omgservers-user/src/main/java/com/omgservers/module/user/impl/operation/validateCredentials/ValidateCredentialsOperation.java
@@ -3,6 +3,13 @@ package com.omgservers.module.user.impl.operation.validateCredentials;
 import com.omgservers.model.user.UserModel;
 import io.smallrye.mutiny.Uni;
 
+import java.time.Duration;
+
 public interface ValidateCredentialsOperation {
     Uni<UserModel> validateCredentials(UserModel user, String password);
+
+    default UserModel validateCredentials(long timeout, UserModel user, String password) {
+        return validateCredentials(user, password)
+                .await().atMost(Duration.ofSeconds(timeout));
+    }
 }
--- omgservers-user/src/main/java/com/omgservers/module/user/impl/operation/validateCredentials/ValidateCredentialsOperationImpl.java
+++ omgservers-user/src/main/java/com/omgservers/module/user/impl/operation/validateCredentials/ValidateCredentialsOperationImpl.java
@@ -1,5 +1,6 @@
 package com.omgservers.module.user.impl.operation.validateCredentials;
 
+import com.omgservers.exception.ServerSideBadRequestException;
 import com.omgservers.exception.ServerSideUnauthorizedException;
 import com.omgservers.model.user.UserModel;
 import io.quarkus.elytron.security.common.BcryptUtil;
@@ -18,6 +19,13 @@ class ValidateCredentialsOperationImpl implements ValidateCredentialsOperation {
     @Override
     public Uni<UserModel> validateCredentials(final UserModel user,
                                               final String password) {
+        if (user == null) {
+            throw new ServerSideBadRequestException("user is null");
+        }
+        if (password == null) {
+            throw new ServerSideBadRequestException("password is null");
+        }
+
         if (BcryptUtil.matches(password, user.getPasswordHash())) {
             return Uni.createFrom().item(user);
         } else {
