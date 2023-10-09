ce2ba6218e870ec109ed54294aba42a36f8680cb
Kirill Byvshev <k@byv.sh>
2023-10-10 00:34:20 +0400

    Check permissions before firing of kick client event

--- omgservers-runtime/src/main/java/com/omgservers/module/runtime/impl/service/doService/impl/method/doKickClient/DoKickClientMethodImpl.java
+++ omgservers-runtime/src/main/java/com/omgservers/module/runtime/impl/service/doService/impl/method/doKickClient/DoKickClientMethodImpl.java
@@ -4,11 +4,15 @@ import com.omgservers.dto.internal.SyncEventRequest;
 import com.omgservers.dto.internal.SyncEventResponse;
 import com.omgservers.dto.runtime.DoKickClientRequest;
 import com.omgservers.dto.runtime.DoKickClientResponse;
+import com.omgservers.exception.ServerSideForbiddenException;
 import com.omgservers.model.event.body.KickRequestedEventBodyModel;
+import com.omgservers.model.runtimeGrant.RuntimeGrantTypeEnum;
+import com.omgservers.module.runtime.impl.operation.hasRuntimeGrant.HasRuntimeGrantOperation;
 import com.omgservers.module.system.SystemModule;
 import com.omgservers.module.system.factory.EventModelFactory;
 import com.omgservers.operation.checkShard.CheckShardOperation;
 import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.pgclient.PgPool;
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,19 +22,39 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 class DoKickClientMethodImpl implements DoKickClientMethod {
 
-    final CheckShardOperation checkShardOperation;
     final SystemModule systemModule;
 
+    final HasRuntimeGrantOperation hasRuntimeGrantOperation;
+    final CheckShardOperation checkShardOperation;
+
     final EventModelFactory eventModelFactory;
+    final PgPool pgPool;
 
     @Override
     public Uni<DoKickClientResponse> doKickClient(final DoKickClientRequest request) {
         return checkShardOperation.checkShard(request.getRequestShardKey())
-                .flatMap(shard -> {
+                .flatMap(shardModel -> {
+                    final var permission = RuntimeGrantTypeEnum.CLIENT;
                     final var runtimeId = request.getRuntimeId();
                     final var userId = request.getUserId();
                     final var clientId = request.getClientId();
-                    return syncEvent(runtimeId, userId, clientId);
+                    return pgPool.withTransaction(sqlConnection -> hasRuntimeGrantOperation.hasRuntimeGrant(
+                                    sqlConnection,
+                                    shardModel.shard(),
+                                    runtimeId,
+                                    userId,
+                                    clientId,
+                                    permission)
+                            .flatMap(has -> {
+                                if (has) {
+                                    return syncEvent(runtimeId, userId, clientId);
+                                } else {
+                                    throw new ServerSideForbiddenException(String.format("lack of permission, " +
+                                                    "runtimeId=%s, client_id=%s, permission=%s",
+                                            runtimeId, clientId, permission));
+                                }
+                            })
+                    );
                 })
                 .replaceWith(new DoKickClientResponse());
     }
