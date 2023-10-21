b9b2de2e2c6f50f28b4e13d21291ed9a646ee76a
Kirill Byvshev <k@byv.sh>
2023-10-21 21:48:53 +0400

    Cleanup lua requests

--- omgservers-script/src/main/java/com/omgservers/module/script/impl/luaRequest/SignedInLuaRequest.java
+++ omgservers-script/src/main/java/com/omgservers/module/script/impl/luaRequest/SignedInLuaRequest.java
@@ -1,6 +1,5 @@
 package com.omgservers.module.script.impl.luaRequest;
 
-import com.omgservers.module.script.impl.luaRequest.LuaRequest;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
