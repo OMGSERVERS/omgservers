02742fd56716f887e03089747f00508595b7005a
Kirill Byvshev <k@byv.sh>
2024-02-25 01:51:34 +0400

    Activate relay job after service bootstraping

--- omgservers-service/src/main/java/com/omgservers/service/ServiceConfiguration.java
+++ omgservers-service/src/main/java/com/omgservers/service/ServiceConfiguration.java
@@ -5,4 +5,5 @@ public class ServiceConfiguration {
     public static final int START_UP_DOCKER_CLIENT_PRIORITY = 1000;
     public static final int START_UP_SCHEMA_MIGRATION_PRIORITY = 1000;
     public static final int START_UP_BOOTSTRAP_SERVICE_PRIORITY = 2000;
+    public static final int START_UP_BOOTSTRAP_RELAY_JOB = 3000;
 }
--- omgservers-service/src/main/java/com/omgservers/service/module/system/impl/component/relayJob/RelayJobImpl.java
+++ omgservers-service/src/main/java/com/omgservers/service/module/system/impl/component/relayJob/RelayJobImpl.java
@@ -2,22 +2,49 @@ package com.omgservers.service.module.system.impl.component.relayJob;
 
 import com.omgservers.model.dto.system.RelayEventsRequest;
 import com.omgservers.model.dto.system.RelayEventsResponse;
+import com.omgservers.service.ServiceConfiguration;
 import com.omgservers.service.module.system.SystemModule;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import io.quarkus.runtime.StartupEvent;
 import io.quarkus.scheduler.Scheduled;
+import io.quarkus.scheduler.ScheduledExecution;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
+import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 @ApplicationScoped
 @AllArgsConstructor
-public class RelayJobImpl implements RelayJob {
+public class RelayJobImpl implements RelayJob, Scheduled.SkipPredicate {
 
     final SystemModule systemModule;
+    final AtomicBoolean skipExecution;
 
-    @Scheduled(every = "1s", concurrentExecution = Scheduled.ConcurrentExecution.SKIP)
+    public RelayJobImpl(final SystemModule systemModule) {
+        this.systemModule = systemModule;
+        this.skipExecution = new AtomicBoolean(true);
+    }
+
+    @WithSpan
+    void startup(@Observes @Priority(ServiceConfiguration.START_UP_BOOTSTRAP_RELAY_JOB) final StartupEvent event) {
+        skipExecution.set(false);
+        log.info("Relay job was activated");
+    }
+
+    @Override
+    public boolean test(ScheduledExecution execution) {
+        return skipExecution.get();
+    }
+
+    @Scheduled(every = "1s",
+            concurrentExecution = Scheduled.ConcurrentExecution.SKIP,
+            skipExecutionIf = RelayJobImpl.class)
     Uni<Void> relayJob() {
         return Multi.createBy().repeating()
                 .uni(this::relayEvents)
