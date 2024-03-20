0fee68644aeeb04f033d86f0ed7a058ab418657f
Kirill Byvshev <k@byv.sh>
2024-03-21 00:04:59 +0400

    Fixed stop matchmaking IT

---
+++ omgservers-tester/src/test/java/com/omgservers/tester/match/MatchStopMatchmakingIT.java
@@ -0,0 +1,129 @@
+package com.omgservers.tester.match;
+
+import com.omgservers.model.message.MessageQualifierEnum;
+import com.omgservers.model.message.body.AssignmentMessageBodyModel;
+import com.omgservers.model.message.body.ServerMessageBodyModel;
+import com.omgservers.model.version.VersionConfigModel;
+import com.omgservers.model.version.VersionGroupModel;
+import com.omgservers.model.version.VersionModeModel;
+import com.omgservers.tester.component.AdminApiTester;
+import com.omgservers.tester.component.PlayerApiTester;
+import com.omgservers.tester.operation.bootstrapTestClient.BootstrapTestClientOperation;
+import com.omgservers.tester.operation.bootstrapTestVersion.BootstrapTestVersionOperation;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+@Slf4j
+@QuarkusTest
+public class MatchStopMatchmakingIT extends Assertions {
+
+    @Inject
+    BootstrapTestVersionOperation bootstrapTestVersionOperation;
+
+    @Inject
+    BootstrapTestClientOperation bootstrapTestClientOperation;
+
+    @Inject
+    PlayerApiTester playerApiTester;
+
+    @Inject
+    AdminApiTester adminApiTester;
+
+    @Test
+    void matchStopMatchmakingIT() throws Exception {
+        final var testVersion = bootstrapTestVersionOperation.bootstrapTestVersion("""                       
+                        function handle_command(self, command)
+                        end
+                        """,
+                """
+                        function handle_command(self, command)
+                                                
+                            if command.qualifier == "add_client" then
+                                return {
+                                    {
+                                        qualifier = "stop_matchmaking",
+                                        reason = "why not?"
+                                    },
+                                    {
+                                        qualifier = "respond_client",
+                                        client_id = command.client_id,
+                                        message = {
+                                            text = "matchmaking_was_stop"
+                                        }
+                                    }
+                                }
+                            end
+                        end
+                        """,
+                new VersionConfigModel(new ArrayList<>() {{
+                    add(VersionModeModel.create("test", 1, 16, new ArrayList<>() {{
+                        add(new VersionGroupModel("players", 1, 16));
+                    }}));
+                }})
+        );
+
+        Thread.sleep(10_000);
+
+        try {
+            final var testClient1 = bootstrapTestClientOperation.bootstrapTestClient(testVersion);
+
+            final var welcomeMessage1 = playerApiTester.waitMessage(testClient1,
+                    MessageQualifierEnum.WELCOME_MESSAGE);
+
+            final var lobbyAssignment1 = playerApiTester.waitMessage(testClient1,
+                    MessageQualifierEnum.ASSIGNMENT_MESSAGE,
+                    Collections.singletonList(welcomeMessage1.getId()));
+
+            playerApiTester.requestMatchmaking(testClient1, "test");
+
+            final var matchAssignment1 = playerApiTester.waitMessage(testClient1,
+                    MessageQualifierEnum.ASSIGNMENT_MESSAGE,
+                    Collections.singletonList(lobbyAssignment1.getId()));
+
+            final var runtimeId1 = ((AssignmentMessageBodyModel) matchAssignment1.getBody()).getRuntimeId();
+
+            final var serverMessage1 = playerApiTester.waitMessage(testClient1,
+                    MessageQualifierEnum.SERVER_MESSAGE,
+                    Collections.singletonList(matchAssignment1.getId()));
+
+            assertEquals("{text=matchmaking_was_stop}",
+                    ((ServerMessageBodyModel) serverMessage1.getBody()).getMessage().toString());
+
+
+            final var testClient2 = bootstrapTestClientOperation.bootstrapTestClient(testVersion);
+
+            final var welcomeMessage2 = playerApiTester.waitMessage(testClient2,
+                    MessageQualifierEnum.WELCOME_MESSAGE);
+
+            final var lobbyAssignment2 = playerApiTester.waitMessage(testClient2,
+                    MessageQualifierEnum.ASSIGNMENT_MESSAGE,
+                    Collections.singletonList(welcomeMessage2.getId()));
+
+            playerApiTester.requestMatchmaking(testClient2, "test");
+
+            final var matchAssignment2 = playerApiTester.waitMessage(testClient2,
+                    MessageQualifierEnum.ASSIGNMENT_MESSAGE,
+                    Collections.singletonList(lobbyAssignment2.getId()));
+
+            final var runtimeId2 = ((AssignmentMessageBodyModel) matchAssignment2.getBody()).getRuntimeId();
+
+            final var serverMessage2 = playerApiTester.waitMessage(testClient2,
+                    MessageQualifierEnum.SERVER_MESSAGE,
+                    Collections.singletonList(matchAssignment1.getId()));
+
+            assertEquals("{text=matchmaking_was_stop}",
+                    ((ServerMessageBodyModel) serverMessage2.getBody()).getMessage().toString());
+
+            assertNotEquals(runtimeId1, runtimeId2);
+
+        } finally {
+            adminApiTester.deleteTenant(testVersion.getTenantId());
+        }
+    }
+}
