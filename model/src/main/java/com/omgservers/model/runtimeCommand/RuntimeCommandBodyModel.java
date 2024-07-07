6c25c63d5aed19eb8dcc6976b8ce23ebb2cdf1a6
Kirill Byvshev <k@byv.sh>
2024-07-07 10:36:33 +0400

    Ignoring qualifier method of command body

--- model/src/main/java/com/omgservers/model/runtimeCommand/RuntimeCommandBodyModel.java
+++ model/src/main/java/com/omgservers/model/runtimeCommand/RuntimeCommandBodyModel.java
@@ -1,6 +1,9 @@
 package com.omgservers.model.runtimeCommand;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public abstract class RuntimeCommandBodyModel {
 
+    @JsonIgnore
     public abstract RuntimeCommandQualifierEnum getQualifier();
 }
