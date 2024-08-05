72db8780a2b77e820ddf2be4d2cbd5aa7a28fd87
Kirill Byvshev <k@byv.sh>
2024-08-05 23:45:15 +0400

    Removed update runtime command

--- schema/src/main/java/com/omgservers/schema/model/runtimeCommand/RuntimeCommandQualifierEnum.java
+++ schema/src/main/java/com/omgservers/schema/model/runtimeCommand/RuntimeCommandQualifierEnum.java
@@ -5,25 +5,17 @@ import com.omgservers.schema.model.runtimeCommand.body.AddMatchClientRuntimeComm
 import com.omgservers.schema.model.runtimeCommand.body.DeleteClientRuntimeCommandBodyModel;
 import com.omgservers.schema.model.runtimeCommand.body.HandleMessageRuntimeCommandBodyModel;
 import com.omgservers.schema.model.runtimeCommand.body.InitRuntimeCommandBodyModel;
-import com.omgservers.schema.model.runtimeCommand.body.UpdateRuntimeCommandBodyModel;
-import com.omgservers.schema.model.runtimeCommand.body.AddClientRuntimeCommandBodyModel;
-import com.omgservers.schema.model.runtimeCommand.body.AddMatchClientRuntimeCommandBodyModel;
-import com.omgservers.schema.model.runtimeCommand.body.DeleteClientRuntimeCommandBodyModel;
-import com.omgservers.schema.model.runtimeCommand.body.HandleMessageRuntimeCommandBodyModel;
-import com.omgservers.schema.model.runtimeCommand.body.InitRuntimeCommandBodyModel;
-import com.omgservers.schema.model.runtimeCommand.body.UpdateRuntimeCommandBodyModel;
 import lombok.Getter;
 
 @Getter
 public enum RuntimeCommandQualifierEnum {
     INIT_RUNTIME(InitRuntimeCommandBodyModel.class),
-    UPDATE_RUNTIME(UpdateRuntimeCommandBodyModel.class),
     ADD_CLIENT(AddClientRuntimeCommandBodyModel.class),
     ADD_MATCH_CLIENT(AddMatchClientRuntimeCommandBodyModel.class),
     DELETE_CLIENT(DeleteClientRuntimeCommandBodyModel.class),
     HANDLE_MESSAGE(HandleMessageRuntimeCommandBodyModel.class);
 
-    Class<? extends RuntimeCommandBodyModel> bodyClass;
+    final Class<? extends RuntimeCommandBodyModel> bodyClass;
 
     RuntimeCommandQualifierEnum(Class<? extends RuntimeCommandBodyModel> bodyClass) {
         this.bodyClass = bodyClass;
