93e2836e981e89bd819889ec49591417a011c1c0
Kirill Byvshev <k@byv.sh>
2024-09-07 18:23:14 +0400

    Fixed omgserversctl -> omgserversctl.sh

--- omgservers-sidecontainers/ctl-sidecontainer/src/main/docker/bootstrap.sh
+++ omgservers-sidecontainers/ctl-sidecontainer/src/main/docker/bootstrap.sh
@@ -6,23 +6,23 @@ if [ -n "${DELAY}" ]; then
 fi
 
 if [ -n "${EXTERNAL_URL}" -o -n "${INTERNAL_URL}" ]; then
-   ./omgserversctl environment useEnvironment default ${EXTERNAL_URL} ${INTERNAL_URL}; \
+   ./omgserversctl.sh environment useEnvironment default ${EXTERNAL_URL} ${INTERNAL_URL}; \
 fi
 
 if [ -n "${ADMIN_USER_ID}" -o -n "${ADMIN_PASSWORD}" ]; then
-  ./omgserversctl admin useCredentials ${ADMIN_USER_ID} ${ADMIN_PASSWORD}; \
+  ./omgserversctl.sh admin useCredentials ${ADMIN_USER_ID} ${ADMIN_PASSWORD}; \
 fi
 
 if [ -n "${SUPPORT_USER_ID}" -o -n "${SUPPORT_PASSWORD}" ]; then
-  ./omgserversctl support useCredentials ${SUPPORT_USER_ID} ${SUPPORT_PASSWORD}; \
+  ./omgserversctl.sh support useCredentials ${SUPPORT_USER_ID} ${SUPPORT_PASSWORD}; \
 fi
 
 if [ -n "${DEVELOPER_USER_ID}" -o -n "${DEVELOPER_PASSWORD}" ]; then
- ./omgserversctl developer useCredentials ${DEVELOPER_USER_ID} ${DEVELOPER_PASSWORD}
+ ./omgserversctl.sh developer useCredentials ${DEVELOPER_USER_ID} ${DEVELOPER_PASSWORD}
 fi
 
 if [ -n "${BACKGROUND}" ]; then
   tail -f /dev/null
 else
-  ./omgserversctl $@
+  ./omgserversctl.sh $@
 fi
--- omgservers-sidecontainers/ctl-sidecontainer/src/main/docker/create_demo_tenant.sh
+++ omgservers-sidecontainers/ctl-sidecontainer/src/main/docker/create_demo_tenant.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 set -e
 
-./omgserversctl support createToken
-./omgserversctl support createTenant
+./omgserversctl.sh support createToken
+./omgserversctl.sh support createTenant
 
-TENANT_ID=$(./omgserversctl environment printVariable TENANT_ID)
+TENANT_ID=$(./omgserversctl.sh environment printVariable TENANT_ID)
 if [ -z "${TENANT_ID}" ]; then
   echo "TENANT_ID was not found"
   exit 1
 fi
 
-./omgserversctl support createProject ${TENANT_ID}
+./omgserversctl.sh support createProject ${TENANT_ID}
 
-PROJECT_ID=$(./omgserversctl environment printVariable PROJECT_ID)
+PROJECT_ID=$(./omgserversctl.sh environment printVariable PROJECT_ID)
 if [ -z "${PROJECT_ID}" ]; then
   echo "PROJECT_ID was not found"
   exit 1
 fi
 
-STAGE_ID=$(./omgserversctl environment printVariable STAGE_ID)
+STAGE_ID=$(./omgserversctl.sh environment printVariable STAGE_ID)
 if [ -z "${STAGE_ID}" ]; then
   echo "STAGE_ID was not found"
   exit 1
 fi
 
-STAGE_SECRET=$(./omgserversctl environment printVariable STAGE_SECRET)
+STAGE_SECRET=$(./omgserversctl.sh environment printVariable STAGE_SECRET)
 if [ -z "${STAGE_SECRET}" ]; then
   echo "STAGE_SECRET was not found"
   exit 1
 fi
 
-./omgserversctl support createDeveloper
+./omgserversctl.sh support createDeveloper
 
-DEVELOPER_USER_ID=$(./omgserversctl environment printVariable DEVELOPER_USER_ID)
+DEVELOPER_USER_ID=$(./omgserversctl.sh environment printVariable DEVELOPER_USER_ID)
 if [ -z "${DEVELOPER_USER_ID}" ]; then
   echo "DEVELOPER_USER_ID was not found"
   exit 1
 fi
 
-DEVELOPER_PASSWORD=$(./omgserversctl environment printVariable DEVELOPER_PASSWORD)
+DEVELOPER_PASSWORD=$(./omgserversctl.sh environment printVariable DEVELOPER_PASSWORD)
 if [ -z "${DEVELOPER_PASSWORD}" ]; then
   echo "DEVELOPER_PASSWORD was not found"
   exit 1
 fi
 
-./omgserversctl support createTenantPermission ${TENANT_ID} ${DEVELOPER_USER_ID} GET_DASHBOARD
-./omgserversctl support createStagePermission ${TENANT_ID} ${STAGE_ID} ${DEVELOPER_USER_ID} VERSION_MANAGEMENT
+./omgserversctl.sh support createTenantPermission ${TENANT_ID} ${DEVELOPER_USER_ID} GET_DASHBOARD
+./omgserversctl.sh support createStagePermission ${TENANT_ID} ${STAGE_ID} ${DEVELOPER_USER_ID} VERSION_MANAGEMENT
 
-./omgserversctl developer useCredentials ${DEVELOPER_USER_ID} ${DEVELOPER_PASSWORD}
+./omgserversctl.sh developer useCredentials ${DEVELOPER_USER_ID} ${DEVELOPER_PASSWORD}
 
 echo
 echo Demo tenant was created:
