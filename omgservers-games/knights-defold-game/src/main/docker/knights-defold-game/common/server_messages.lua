8048ccbeecc47888ae7593d26cd4f859d9986517
Kirill Byvshev <k@byv.sh>
2024-11-05 00:37:41 +0400

    Simplified match state model

--- omgservers-games/knights-defold-game/src/main/docker/knights-defold-game/common/server_messages.lua
+++ omgservers-games/knights-defold-game/src/main/docker/knights-defold-game/common/server_messages.lua
@@ -2,7 +2,6 @@ local server_messages
 server_messages = {
 	SET_PROFILE = "set_profile",
 	SET_STATE = "set_state",
-	REQUEST_MOVE = "request_move",
 	APPLY_EVENTS = "apply_events",
 	-- Methods
 	set_profile = function(self, profile)
@@ -11,17 +10,13 @@ server_messages = {
 			profile = profile,
 		}
 	end,
-	set_state = function(self, settings, dangling_players, spawned_players)
+	set_state = function(self, step, settings, dangling_players, spawned_players)
 		return {
 			qualifier = server_messages.SET_STATE,
+			step = step,
 			settings = settings,
 			dangling_players = dangling_players,
 			spawned_players = spawned_players,
-		}
-	end,
-	request_move = function(self)
-		return {
-			qualifier = server_messages.REQUEST_MOVE,
 		}
 	end,
 	apply_events = function(self, events)
--- omgservers-games/knights-defold-game/src/main/docker/knights-defold-game/game/match_screen/match_screen.lua
+++ omgservers-games/knights-defold-game/src/main/docker/knights-defold-game/game/match_screen/match_screen.lua
@@ -2,7 +2,6 @@ local match_screen
 match_screen = {
 	SET_STATE = "set_state",
 	APPLY_EVENTS = "apply_events",
-	REQUEST_MOVE = "request_move",
 	POINTED = "pointed",
 	-- Methods
 	set_state = function(self, recipient, settings, dangling_players, spawned_players)
@@ -15,10 +14,6 @@ match_screen = {
 	apply_events = function(self, recipient, events)
 		msg.post(recipient, match_screen.APPLY_EVENTS, {
 			events = events,
-		})
-	end,
-	request_move = function(self, recipient)
-		msg.post(recipient, match_screen.REQUEST_MOVE, {
 		})
 	end,
 	pointed = function(self, recipient, x, y)
