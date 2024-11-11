63488e24c7e01bce67a3b5941f2296c753a68843
Kirill Byvshev <k@byv.sh>
2024-11-12 00:32:09 +0400

    Using simple graphic for test game project

--- omgservers-games/knights-defold-game/src/main/docker/knights-defold-game/server/match_runtime/match_settings.lua
+++ omgservers-games/knights-defold-game/src/main/docker/knights-defold-game/server/match_runtime/match_settings.lua
@@ -2,13 +2,13 @@ local match_settings
 match_settings = {
 	constants = {
 		MATCH_LIFETIME = 256,
-		MATCH_WIDTH = 512,
-		MATCH_HEIGHT = 512,
-		MATCH_BORDER = 32,
+		MATCH_WIDTH = 256,
+		MATCH_HEIGHT = 256,
+		MATCH_BORDER = 16,
 		SPAWN_STEPS = 2,
 		SIMULATION_STEP = 16,
 		SIMULATION_OFFSET = 4,
-		PLAYER_SIZE = 32,
+		PLAYER_SIZE = 16,
 		SIMULATION_INTERVAL = 2,
 	},
 }
