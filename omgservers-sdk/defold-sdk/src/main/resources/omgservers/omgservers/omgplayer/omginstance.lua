db9f2720a9e8ff3c1be57a865ad36e6f748ce034
Kirill Byvshev <k@byv.sh>
2025-01-06 00:40:22 +0400

    To interact with the service, an omginstance must first be created.

--- omgservers-sdk/defold-sdk/src/main/resources/omgservers/omgservers/omgplayer/omginstance.lua
+++ omgservers-sdk/defold-sdk/src/main/resources/omgservers/omgservers/omgplayer/omginstance.lua
@@ -8,120 +8,127 @@ local omgprocess = require("omgservers.omgplayer.omgprocess")
 
 local omginstance
 omginstance = {
-	config = nil,
-	http = nil,
-	events = nil,
-	client = nil,
-	dispatcher = nil,
-	process = nil,
-	-- Methods
-	init = function(self, options)
-		assert(self, "The self must not be nil.")
-		assert(options, "The options must not be nil.")
-
-		self.config = omgconfig:create(options)
-		self:reset()
-	end,
-	reset = function(self)
+	create = function(self)
 		assert(self, "The self must not be nil.")
 
-		if self.dispatcher then
-			self.dispatcher:disconnect()
-		end
+		return {
+			type = "omginstance",
+			config = nil,
+			http = nil,
+			events = nil,
+			client = nil,
+			dispatcher = nil,
+			process = nil,
+			-- Methods
+			init = function(instance, options)
+				assert(instance, "The self must not be nil.")
+				assert(options, "The options must not be nil.")
 
-		self.events = omgevents:create({
-			config = self.config,
-		})
+				instance.config = omgconfig:create(options)
+				instance:reset()
+			end,
+			reset = function(instance)
+				assert(instance, "The self must not be nil.")
 
-		self.state = omgstate:create({
-			events = self.events,
-		})
+				if instance.dispatcher then
+					instance.dispatcher:disconnect()
+				end
 
-		self.http = omghttp:create({
-			config = self.config,
-		})
+				instance.events = omgevents:create({
+					config = instance.config,
+				})
 
-		self.client = omgclient:create({
-			config = self.config,
-			state = self.state,
-			http = self.http,
-		})
+				instance.state = omgstate:create({
+					events = instance.events,
+				})
 
-		self.dispatcher = omgdispatcher:create({
-			config = self.config,
-			events = self.events,
-			state = self.state,
-		})
+				instance.http = omghttp:create({
+					config = instance.config,
+				})
 
-		self.process = omgprocess:create({
-			config = self.config,
-			events = self.events,
-			state = self.state,
-			client = self.client,
-			dispatcher = self.dispatcher,
-		})
-	end,
-	ping = function(self)
-		assert(self, "The self must not be nil.")
-		assert(self.config, "The player must be initialized")
-		assert(not self.state.failed, "The player has failed, a reset is required")
+				instance.client = omgclient:create({
+					config = instance.config,
+					state = instance.state,
+					http = instance.http,
+				})
 
-		self.client:ping_service(function(latency, message)
-			self.events:service_pinged(latency)
-		end)
-	end,
-	sign_up = function(self)
-		assert(self, "The self must not be nil.")
-		assert(self.config, "The player must be initialized")
-		assert(not self.state.failed, "The player has failed, a reset is required")
+				instance.dispatcher = omgdispatcher:create({
+					config = instance.config,
+					events = instance.events,
+					state = instance.state,
+				})
 
-		self.client:create_user(function(user_id, password)
-			self.events:signed_up(user_id, password)
-		end)
-	end,
-	sign_in = function(self, user_id, password)
-		assert(self, "The self must not be nil.")
-		assert(user_id, "The user_id must not be nil.")
-		assert(password, "The password must not be nil.")
-		assert(self.config, "The player must be initialized")
-		assert(not self.state.failed, "The player has failed, a reset is required")
+				instance.process = omgprocess:create({
+					config = instance.config,
+					events = instance.events,
+					state = instance.state,
+					client = instance.client,
+					dispatcher = instance.dispatcher,
+				})
+			end,
+			ping = function(instance)
+				assert(instance, "The self must not be nil.")
+				assert(instance.config, "The player must be initialized")
+				assert(not instance.state.failed, "The player has failed, a reset is required")
 
-		self.client:set_user(user_id, password)
+				instance.client:ping_service(function(latency, message)
+					instance.events:service_pinged(latency)
+				end)
+			end,
+			sign_up = function(instance)
+				assert(instance, "The self must not be nil.")
+				assert(instance.config, "The player must be initialized")
+				assert(not instance.state.failed, "The player has failed, a reset is required")
 
-		self.client:create_token(function(api_token)
-			self.client:create_client(function(client_id)
-				self.events:signed_in(client_id)
-			end)
-		end)
-	end,
-	send_service_message = function(self, message)
-		assert(self, "The self must not be nil.")
-		assert(message, "The message must not be nil.")
-		assert(self.config, "The player must be initialized")
-		assert(not self.state.failed, "The player has failed, a reset is required")
+				instance.client:create_user(function(user_id, password)
+					instance.events:signed_up(user_id, password)
+				end)
+			end,
+			sign_in = function(instance, user_id, password)
+				assert(instance, "The self must not be nil.")
+				assert(user_id, "The user_id must not be nil.")
+				assert(password, "The password must not be nil.")
+				assert(instance.config, "The player must be initialized")
+				assert(not instance.state.failed, "The player has failed, a reset is required")
 
-		self.client:send_message(message)
-	end,
-	send_text_message = function(self, message)
-		assert(self, "The self must not be nil.")
-		assert(message, "The message must not be nil.")
-		assert(self.config, "The player must be initialized")
-		assert(not self.state.failed, "The player has failed, a reset is required")
+				instance.client:set_user(user_id, password)
 
-		self.dispatcher:send_text_message(message)
-	end,
-	send_binary_message = function(self, buffer)
-		assert(self, "The self must not be nil.")
-		assert(buffer, "The buffer must not be nil.")
-		assert(self.config, "The player must be initialized")
-		assert(not self.state.failed, "The player has failed, a reset is required")
+				instance.client:create_token(function(api_token)
+					instance.client:create_client(function(client_id)
+						instance.events:signed_in(client_id)
+					end)
+				end)
+			end,
+			send_service_message = function(instance, message)
+				assert(instance, "The self must not be nil.")
+				assert(message, "The message must not be nil.")
+				assert(instance.config, "The player must be initialized")
+				assert(not instance.state.failed, "The player has failed, a reset is required")
 
-		self.dispatcher:send_binary_buffer(buffer)
-	end,
-	update = function(self, dt)
-		assert(self, "The self must not be nil.")
-		assert(self.config, "The player must be initialized")
-		self.process:update(dt)
+				instance.client:send_message(message)
+			end,
+			send_text_message = function(instance, message)
+				assert(instance, "The self must not be nil.")
+				assert(message, "The message must not be nil.")
+				assert(instance.config, "The player must be initialized")
+				assert(not instance.state.failed, "The player has failed, a reset is required")
+
+				instance.dispatcher:send_text_message(message)
+			end,
+			send_binary_message = function(instance, buffer)
+				assert(instance, "The self must not be nil.")
+				assert(buffer, "The buffer must not be nil.")
+				assert(instance.config, "The player must be initialized")
+				assert(not instance.state.failed, "The player has failed, a reset is required")
+
+				instance.dispatcher:send_binary_buffer(buffer)
+			end,
+			update = function(instance, dt)
+				assert(instance, "The self must not be nil.")
+				assert(instance.config, "The player must be initialized")
+				instance.process:update(dt)
+			end,
+		}
 	end,
 }
 
--- omgservers-sdk/defold-sdk/src/main/resources/omgservers/omgservers/omgplayer/omgplayer.lua
+++ omgservers-sdk/defold-sdk/src/main/resources/omgservers/omgservers/omgplayer/omgplayer.lua
@@ -5,33 +5,10 @@ local omgplayer
 omgplayer = {
 	constants = omgconstants,
 	-- Methods
-	init = function(self, options)
-		omginstance:init(options)
-	end,
-	reset = function(self)
-		omginstance:reset()
-	end,
-	ping = function(self)
-		omginstance:ping()
-	end,
-	sign_up = function(self)
-		omginstance:sign_up()
-	end,
-	sign_in = function(self, user_id, password)
-		omginstance:sign_in(user_id, password)
-	end,
-	send_service_message = function(self, message)
-		omginstance:send_service_message(message)
-	end,
-	send_text_message = function(self, message)
-		omginstance:send_text_message(message)
-	end,
-	send_binary_message = function(self, buffer)
-		omginstance:send_binary_message(buffer)
-	end,
-	update = function(self, dt)
-		omginstance:update(dt)
-	end,
+	create = function(self)
+		local instance = omginstance:create()
+		return instance
+	end
 }
 
 return omgplayer
