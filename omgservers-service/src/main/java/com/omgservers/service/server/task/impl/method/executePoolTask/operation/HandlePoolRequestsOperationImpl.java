aa03ed75f903a37ecbcf029e42c247892a211e77
Kirill Byvshev <k@byv.sh>
2025-06-06 10:14:56 +0400

    Delete pool requests if the container needs to be deleted

--- omgservers-service/src/main/java/com/omgservers/service/server/task/impl/method/executePoolTask/operation/HandlePoolRequestsOperationImpl.java
+++ omgservers-service/src/main/java/com/omgservers/service/server/task/impl/method/executePoolTask/operation/HandlePoolRequestsOperationImpl.java
@@ -19,19 +19,26 @@ class HandlePoolRequestsOperationImpl implements HandlePoolRequestsOperation {
                         final HandlePoolResult handlePoolResult) {
         final var poolScheduler = createPoolSchedulerOperation.execute(fetchPoolResult);
 
-        fetchPoolResult.poolState().getPoolRequests().forEach(poolRequest -> {
-            final var poolServerOptional = poolScheduler.schedule(poolRequest);
-            if (poolServerOptional.isPresent()) {
-                final var poolServer = poolServerOptional.get();
-                final var poolContainer = createPoolContainerOperation.execute(poolServer, poolRequest);
+        fetchPoolResult.poolState().getPoolRequests()
+                .forEach(poolRequest -> {
+                    final var poolRequestId = poolRequest.getId();
+                    final var isNotForDelete = !handlePoolResult.poolChangeOfState().getPoolRequestsToDelete()
+                            .contains(poolRequestId);
 
-                handlePoolResult.poolChangeOfState().getPoolContainersToSync().add(poolContainer);
-                handlePoolResult.poolChangeOfState().getPoolRequestsToDelete().add(poolRequest.getId());
-            } else {
-                log.error("Failed to schedule container, request={}/{}",
-                        poolRequest.getPoolId(), poolRequest.getId());
-            }
-        });
+                    if (isNotForDelete) {
+                        final var poolServerOptional = poolScheduler.schedule(poolRequest);
+                        if (poolServerOptional.isPresent()) {
+                            final var poolServer = poolServerOptional.get();
+                            final var poolContainer = createPoolContainerOperation.execute(poolServer, poolRequest);
+
+                            handlePoolResult.poolChangeOfState().getPoolContainersToSync().add(poolContainer);
+                            handlePoolResult.poolChangeOfState().getPoolRequestsToDelete().add(poolRequest.getId());
+                        } else {
+                            log.error("Failed to schedule container, request={}/{}",
+                                    poolRequest.getPoolId(), poolRequest.getId());
+                        }
+                    }
+                });
     }
 
 }
