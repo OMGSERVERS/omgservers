e067cb6d73de56fa70bf32a3ff35b32baac73d49
Kirill Byvshev <k@byv.sh>
2025-06-07 13:14:24 +0400

    Fixed logging of install script

--- install.sh
+++ install.sh
@@ -7,9 +7,10 @@ DOWNLOAD_URL="https://github.com/OMGSERVERS/omgservers/releases/download/${VERSI
 ARCHIVE_NAME=$(basename ${DOWNLOAD_URL})
 TEMP_DIR=$(mktemp -d)
 
-echo "Getting ${DOWNLOAD_URL}"
-curl -L ${DOWNLOAD_URL} -o "${TEMP_DIR}/${ARCHIVE_NAME}"
+echo "Getting from ${DOWNLOAD_URL}"
+curl -sSL --fail-with-body ${DOWNLOAD_URL} -o "${TEMP_DIR}/${ARCHIVE_NAME}"
+
+echo "Extracting to ${TEMP_DIR}"
 unzip -q "${TEMP_DIR}/${ARCHIVE_NAME}" -d "${TEMP_DIR}"
 
-echo "Bring up local testing environment"
 docker compose -f "${TEMP_DIR}/localtesting-environment/compose.yaml" up -d
