6756060d31e0e9c7ea485f26c629c7fc6e455a1d
Kirill Byvshev <k@byv.sh>
2025-06-19 23:56:12 +0400

    Encoding the sub-protocol value as a URI component to avoid encoding issues (#210)

--- omgservers-service/src/main/java/com/omgservers/service/operation/security/CreateQuarkusHeaderProtocolOperationImpl.java
+++ omgservers-service/src/main/java/com/omgservers/service/operation/security/CreateQuarkusHeaderProtocolOperationImpl.java
@@ -6,6 +6,9 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @Slf4j
 @ApplicationScoped
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
@@ -13,8 +16,10 @@ class CreateQuarkusHeaderProtocolOperationImpl implements CreateQuarkusHeaderPro
 
     @Override
     public String execute(final String wsToken) {
-        final var quarkusHeaderProtocol = WebSocketConfiguration.SUB_PROTOCOL +
-                ",quarkus-http-upgrade#Authorization#Bearer " + wsToken;
+        final var quarkusHttpUpgrade = "quarkus-http-upgrade#Authorization#Bearer " + wsToken;
+        final var encodedHttpUpgrade = URLEncoder.encode(quarkusHttpUpgrade, StandardCharsets.UTF_8);
+
+        final var quarkusHeaderProtocol = WebSocketConfiguration.SUB_PROTOCOL + "," + encodedHttpUpgrade;
         return quarkusHeaderProtocol;
     }
 }
